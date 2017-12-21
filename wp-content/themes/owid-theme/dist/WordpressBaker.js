0d394864fe4e5ef5f089d05d26739f88eebdc2d5
Jaiden Mispy <jaiden@mispy.me>
2017-12-21 13:21:41 +0800

    Automatic update

--- wp-content/themes/owid-theme/dist/WordpressBaker.js
+++ wp-content/themes/owid-theme/dist/WordpressBaker.js
@@ -159,6 +159,7 @@ var WordpressBaker = /** @class */ (function () {
                             try {
                                 outPath = path.join(outDir, slug + ".html");
                                 stat = fs.statSync(outPath);
+                                console.log(slug, stat.mtime, row.post_modified);
                                 if (stat.mtime >= row.post_modified) {
                                     // No newer version of this post, don't bother to bake
                                     //console.log(`304 ${slug}`)
