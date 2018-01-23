22dad5b6151f20aac69bb2b92bc78893de53600d
Jaiden Mispy <jaiden@mispy.me>
2018-01-23 16:34:13 +0800

    Updating about

--- wp-content/themes/owid-theme/deploy.sh
+++ wp-content/themes/owid-theme/deploy.sh
@@ -6,14 +6,10 @@ if [ "$1" == "test" ]; then
   HOST="owid@terra"
   ENV="test"
   WORDPRESS_DIR="/home/owid/test.ourworldindata.org"
-  WORDPRESS_URL="https://test.ourworldindata.org"
-  WORDPRESS_DB="test_wordpress"
 elif [ "$1" == "live" ]; then
   ENV="live"
   HOST="owid@terra"
   WORDPRESS_DIR="/home/owid/ourworldindata.org"
-  WORDPRESS_URL="https://ourworldindata.org"
-  WORDPRESS_DB="owid_wordpress"
 
   # Prompt for confirmation if deploying to live
   read -p "Are you sure you want to deploy to 'live'? " -n 1 -r
@@ -25,11 +21,14 @@ fi
 
 if [[ $REPLY =~ ^[Yy]$ ]] || [ "$1" != "live" ]
 then
+  ./node_modules/.bin/webpack -p
+
   TMP="/home/owid/tmp"
   OLD_REPO="$TMP/$ENV-owid-theme-old"
   SYNC_TARGET="$TMP/$ENV-owid-theme"
   TMP_NEW="$TMP/$ENV-owid-theme-new"
   FINAL_TARGET="$WORDPRESS_DIR/wp-content/themes/owid-theme"
+  DATA="/home/owid/$ENV-theme-data"
 
   ssh -t $HOST "rm -r $OLD_REPO"
   $RSYNC $DIR/ $HOST:$SYNC_TARGET
@@ -37,7 +36,11 @@ then
     cp -r $SYNC_TARGET $TMP_NEW
     mv $FINAL_TARGET $OLD_REPO
     mv $TMP_NEW $FINAL_TARGET
-    cd $FINAL_TARGET && yarn
-    node dist/deployHook.js $WORDPRESS_DB $WORDPRESS_URL $WORDPRESS_DIR
+
+    ln -sf $DATA/.env $FINAL_TARGET/.env
+
+    cd $FINAL_TARGET
+    yarn
+    node dist/src/deployHook.js
 EOF
 fi
